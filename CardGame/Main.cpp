a1d0ca9b715f45a8685c907b28f1e13a1f44150c
macravenduarte <duarte.marco242@gmail.com>
2016-07-16 18:24:39 -0400

    added point tracking but now there is an infinite loop that needs to be deugged. added game start, cardResult, and gameResult functions

--- CardGame/Main.cpp
+++ CardGame/Main.cpp
@@ -3,30 +3,29 @@
 
 using namespace std;
 //prototype
-void Result(card, card);
+bool gameStart();
+void cardResult(card, card);
+bool getGameResult(bool);
+void addPlayerPoints();
+void addAiPoints();
+int getPlayerPoints();
+int getAiPoints();
+int playerPoints = 0, aiPoints = 0;
 
 int main(){
 
-	deckCards deck;
-	card currentCard;
-	//our added code to for human and ai
-	card humanHand, aiHand;
+	//deckCards deck;
+	//card currentCard;
+	////our added code to for human and ai
+	//card humanHand, aiHand;
 	bool game = true;
 	int selection;
 
 	//start game loop
 	do {
-
-		deck.shuffle();
-		cout << endl << endl;
-
-		humanHand = deck.dealCard();
-		cout << "Your Current hand is " << humanHand.print() << endl;
-		aiHand = deck.dealCard();
-		cout << "Your opponents hand is " << aiHand.print() << endl;
-
-		Result(humanHand, aiHand);
 		
+		gameStart();
+
 		cout << "Do You Want To Play Again?" << endl;
 		cout << "1 = yes, 0 = no" << endl;
 		cin >> selection;
@@ -41,26 +40,99 @@ int main(){
 
 }
 
+//This method will run the game
+bool gameStart() {
+	
+	deckCards deck;
+	card currentCard;
+	//our added code to for human and ai
+	card humanHand, aiHand;
+	bool winner = false;
+	
+	//run a loop that will continue the game 
+	do {
+		deck.shuffle();
+		cout << endl << endl;
+
+		humanHand = deck.dealCard();
+		cout << "Your Current hand is " << humanHand.print() << endl;
+		
+		aiHand = deck.dealCard();
+		cout << "Your opponents hand is " << aiHand.print() << endl;
+
+		cardResult(humanHand, aiHand);	//check who has the winning hand
+		getGameResult(winner);	//check to see who has the most points, best 2 out of 3
+
+	} while(!getGameResult(winner));
+}
+
 //this method will compare the two hands dealt to the user and ai. 
-void Result(card humanHand, card aiHand) {
+void cardResult(card humanHand, card aiHand) {
 
 	//local variable to comare face values
-	int human = humanHand.getFaceValue();
-	int ai = aiHand.getFaceValue();
+	int human = humanHand.getFaceValue(), ai = aiHand.getFaceValue();
 
 	if (human > ai) {
-
-		cout << "Player Wins!!!!" << endl;
+		addPlayerPoints();
+		cout << "Player Scores a Point!!!!" << endl;
+		cout << "Player has " << getPlayerPoints() << "point(s)." << endl;
 	}
 	else if (human < ai) {
-
-		cout << "Ai Wins!!!!" << endl;
-
+		addAiPoints();
+		cout << "Ai Scores a Point!!!!" << endl;
+		cout << "Ai has " << getAiPoints() << "point(s)." << endl;
 	}
 	else {
-
-		cout << "Tie Game but Ai Wins!!!!" << endl;
-
+		cout << "Tie no points awarded!!!!" << endl;
+		cout << "The score is:" << endl;
+		cout << "Player: " << getPlayerPoints() << " point(s)." << endl;
+		cout << "Ai: " << getAiPoints() << " point(s)." << endl;
 	}
 
+}
+
+//This method will get the game result
+bool getGameResult(bool winner) {
+
+	bool result = false; 
+
+	if (playerPoints = 2) {
+		cout << "Player has " << getPlayerPoints() << " point(s)." << endl;
+		cout << "Player Wins!!!!" << endl;
+		result = true;
+		playerPoints = 0;
+	}
+	else if (aiPoints = 2) {
+		cout << "Ai has " << getAiPoints() << " point(s)." << endl;
+		cout << "Ai Wins!!!!" << endl;
+		result = true;
+		aiPoints = 0;
+	}
+	else {
+		result = false;
+	}
+
+	winner = result;
+
+	return winner;
+}
+
+//This method adds a point for the Player
+void addPlayerPoints() {
+	playerPoints++;
+}
+
+//This method adds a point for the Ai
+void addAiPoints() {
+	aiPoints++;
+}
+
+//This method returns the player points
+int getPlayerPoints() {
+	return playerPoints;
+}
+
+//This method returns the Ai points
+int getAiPoints() {
+	return aiPoints;
 }
